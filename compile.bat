2b715f420364fbf206272a34b7950559d453ec66
ndzin <astfdatamining@gmail.com>
2024-01-01 09:47:41 -0300

    update compiler

--- compile.bat
+++ compile.bat
@@ -1,3 +1,10 @@
 @echo off
 python -m PyInstaller --onefile main.py --name "hdiff-patcher" --icon icon-grab.exe --uac-admin
+
+move "dist\hdiff-patcher.exe" .
+
+rmdir /s /q "./dist"
+rmdir /s /q "./build"
+del "hdiff-patcher.spec"
+
 pause
